7b84e666fb967fce14e54995769434819778d8be
BlessingIhejirika <onyinyechukwumihejirika@gmail.com>
2025-10-25 04:39:28 +0100

    commiting changes aded to the file MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,22 +1,21 @@
 import mysql.connector
-from mysql.connector import Error
 from mysql.connector.constants import ClientFlag
 from dotenv import load_dotenv
 import os
 
-
+# Load environment variables
 load_dotenv()
 
 def create_database():
     connection = None
     try:
-        
+        # Load credentials from .env
         host = os.getenv("MYSQL_HOST")
         port = int(os.getenv("MYSQL_PORT"))
         user = os.getenv("MYSQL_USER")
         password = os.getenv("MYSQL_PASSWORD")
 
-        
+        # Connect to MySQL server
         connection = mysql.connector.connect(
             host=host,
             port=port,
@@ -31,7 +30,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
