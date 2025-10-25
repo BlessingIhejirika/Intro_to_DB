c3af168df1f741635ff3d1db6321b0e850e7b4ad
BlessingIhejirika <onyinyechukwumihejirika@gmail.com>
2025-10-25 05:08:47 +0100

    committing changes made to file task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -22,9 +22,9 @@ CREATE TABLE IF NOT EXISTS Books (
 -- Create Customers table
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE,
-    address VARCHAR(255),
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE,
+    address TEXT,
     phone VARCHAR(20)
 );
 
